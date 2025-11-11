21aa6e49f734a9aaa516047e36840c1f61c37817
Jonathan Mejia <jonathanmejiacaro@gmail.com>
2025-11-10 21:30:36 -0400

    agregando carpeta de reporte

--- src/e2e-tests/support/world.ts
+++ src/e2e-tests/support/world.ts
@@ -25,8 +25,8 @@ export class CustomWorld extends World {
     this.baseUrl = process.env.BASE_URL || 'https://www.saucedemo.com';
     this.userStandard = process.env.USER_STANDARD || 'standard_user';
     this.userPassword = process.env.USER_PASSWORD || 'secret_sauce';
-    this.firstName = process.env.FIRST_NAME || 'Jael';
-    this.lastName = process.env.LAST_NAME || 'Diaz';
+    this.firstName = process.env.FIRST_NAME || 'Jonathan';
+    this.lastName = process.env.LAST_NAME || 'Mejia';
     this.postalCode = process.env.POSTAL_CODE || '10101';
   }
 
